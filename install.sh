93999ab7b20c8e1115c4933bdb497380e0dd41e7
Gintaras Sova <gintaras_sova@hotmail.com>
2021-01-10 13:52:50 +0000

    Add bash srcipts install and start

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker-compose down
+
+rm -fr ./src/*
+rm -frv ./src/.* 2>/dev/null
+
+docker-compose run --rm composer create-project --prefer-dist laravel/laravel .
+docker-compose up -d --build nginx php mysql
